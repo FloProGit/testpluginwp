e1dd04644a8e81228d969b12d873cba32ccd1b90
florian <sudreflorianpro@gmail.com>
2023-07-23 18:28:28 +0200

    first commit

---
+++ testplugin.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ Plugin Name: testplugin
+ Plugin Uri:https://github.com/FloProGit/testpluginwp
+    Description: ceci est un test de description !
+Version : 0.1
+Author: Florian Sudre
+ */
